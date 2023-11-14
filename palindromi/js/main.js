3bd22eca6151101bdf8cdab058eeac25858a1a12
antoniodalterio <antoniodalterio2227@gmail.com>
2023-11-14 16:24:28 +0100

    feat: esercizio 1

--- palindromi/js/main.js
+++ palindromi/js/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// Funzioni
+
+function palindromi() {
+  const stringaInversa = parolaUtente.split('').reverse().join('');
+  return stringaInversa;
+}
+
+// Funzioni
+
+// Dichiarazioni variabili
+const parolaUtente = prompt('scrivi una parola');
+const parolaInvertita = palindromi(parolaUtente);
+// Dichiarazioni variabili
+
+if (parolaUtente == parolaInvertita) {
+  console.log('la parola è palindroma');
+} else {
+  console.log('la parola non è palindroma');
+}
