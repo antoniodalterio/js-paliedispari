1f3b42d4fe4ad681dd7e9f7f4eb0100d07f7f529
antoniodalterio <antoniodalterio2227@gmail.com>
2023-11-14 17:25:32 +0100

    feat: esercizio pari e dispari

--- pari e dispari/js/main.js
+++ pari e dispari/js/main.js
@@ -7,15 +7,37 @@
 // Dichiariamo chi ha vinto.
 
 // FUNZIONI
-function generatoreNumero(num1, num2) {
-  const somma = sceltaNumero + numeroPc;
-  console.log(somma);
+function sommaNumero(num1, num2) {
+  let somma = sceltaNumero + numeroPc;
   return somma;
+}
+
+function vittoria() {
+  if (risultatoSomma % 2 == 0) {
+    const pari = alert('pari vince');
+    return pari;
+  } else if (risultatoSomma % 3 == 0) {
+    const dispari = alert('dispari vince');
+    return dispari;
+  }
 }
 
 // FUNZIONI
 
 const sceltaUtente = prompt('scegli tra pari e dispari');
+
+if (sceltaUtente === 'dispari') {
+  alert('hai scelto dispari');
+} else {
+  alert('hai scelto pari');
+}
+
 const sceltaNumero = Number(prompt('scegli un numero da 1 a 5'));
+alert(`hai scelto ${sceltaNumero}`);
 const numeroPc = Math.floor(Math.random() * 5) + 1;
-console.log(numeroPc);
+
+const risultatoSomma = sommaNumero();
+alert(`risultato della somma è ${risultatoSomma}`);
+
+const risultato = vittoria();
+console.log(risultato);
